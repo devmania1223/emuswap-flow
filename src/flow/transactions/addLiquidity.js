310e931b5765b2b9a9940ae62b6a941d9895373b
julianstore <today.julian.lim@gmail.com>
2022-04-29 19:17:11 -0600

    addLiquidity fix

--- src/flow/transactions/addLiquidity.js
+++ src/flow/transactions/addLiquidity.js
@@ -11,67 +11,68 @@ export const addLiquidity = async (signer, token1Amount , token2Amount) => {
         import EmuSwap from 0xEmuSwap
 
         transaction(token1Amount: UFix64, token2Amount: UFix64) {
-
-            // The Vault references that holds the tokens that are being transferred
+  
+            let poolID:UInt64
+            
+            // The Vault references that holds the tokens that are being added as liquidity
             let flowTokenVaultRef: &FlowToken.Vault
             let fusdVaultRef: &FUSD.Vault
-        
-            // EmuSwap Admin Ref
-            let adminRef: &EmuSwap.Admin
             
-            // new pool to deposit to collection
-            let lpTokenVault: @EmuSwap.TokenVault
-        
             // reference to lp collection
-            let lpCollectionRef: &EmuSwap.Collection
-        
-        
-            // the signers auth account to pass to execute block
-            let signer: AuthAccount
-        
+            var lpCollectionRef: &EmuSwap.Collection
+            
+            // The Vault reference for liquidity tokens
+            var liquidityTokenRef: &FungibleTokens.TokenVault
+            
             prepare(signer: AuthAccount) {
+                // perhaps function to look this up instead of hard coding
+                self.poolID = 0
             
-            // prepare tokens refernces to withdraw inital liquidity 
-            self.flowTokenVaultRef = signer.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
-                ?? panic("Could not borrow a reference to Vault")
-        
-            self.fusdVaultRef = signer.borrow<&FUSD.Vault>(from: /storage/fusdVault)
-                ?? panic("Could not borrow a reference to fusd Vault")
-        
-            // Create new Pool Vault 
-            self.lpTokenVault <-EmuSwap.createEmptyTokenVault(tokenID: EmuSwap.nextPoolID) //to: EmuSwap.LPTokensStoragePath
+                self.flowTokenVaultRef = signer.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+                    ?? panic("Could not borrow a reference to Vault")
             
-            // check if Collection is created if not then create
-            if signer.borrow<&EmuSwap.Collection>(from: EmuSwap.LPTokensStoragePath) == nil {
+                self.fusdVaultRef = signer.borrow<&FUSD.Vault>(from: /storage/fusdVault)
+                    ?? panic("Could not borrow a reference to Vault")
+            
+                // check if Collection is created if not then create
+                if signer.borrow<&EmuSwap.Collection>(from: EmuSwap.LPTokensStoragePath) == nil {
                 // Create a new Collection and put it in storage
                 signer.save(<- EmuSwap.createEmptyCollection(), to: EmuSwap.LPTokensStoragePath)
                 
-                
-            }
-            self.lpCollectionRef = signer.borrow<&EmuSwap.Collection>(from: EmuSwap.LPTokensStoragePath)!
-        
-            self.adminRef = signer.borrow<&EmuSwap.Admin>(from: EmuSwap.AdminStoragePath)
-                ?? panic("Could not borrow a reference to EmuSwap Admin")
-        
-            self.signer = signer
-            }
-        
-            execute {
-            // Withdraw tokens
-            let token1Vault <- self.flowTokenVaultRef.withdraw(amount: token1Amount) as! @FlowToken.Vault
-            let token2Vault <- self.fusdVaultRef.withdraw(amount: token2Amount) as! @FUSD.Vault
-        
-            // Provide liquidity and get liquidity provider tokens
-            let tokenBundle <- EmuSwap.createTokenBundle(fromToken1: <- token1Vault, fromToken2: <- token2Vault)
-        
-            // Keep the liquidity provider tokens
-            let lpTokens <- self.adminRef.createNewLiquidityPool(from: <- tokenBundle)
-        
-            self.adminRef.togglePoolFreeze(id: lpTokens.tokenID)
+                signer.link<&{FungibleTokens.CollectionPublic}>(
+                    EmuSwap.LPTokensPublicReceiverPath, 
+                    target: EmuSwap.LPTokensStoragePath
+                )
+                let lpTokensReceiverCap = signer.getCapability<&{FungibleTokens.CollectionPublic}>(EmuSwap.LPTokensPublicReceiverPath)
+                }
             
-            self.lpTokenVault.deposit(from: <-lpTokens )
-            self.lpCollectionRef.deposit(token: <- self.lpTokenVault)
-            //self.signer.save(<- lpTokens, to: /storage/LPToken)
+                // store reference to LP Tokens Collection
+                self.lpCollectionRef = signer.borrow<&EmuSwap.Collection>(from: EmuSwap.LPTokensStoragePath)!
+            
+                // check if the user has the correct LP in their collection
+                if !self.lpCollectionRef.getIDs().contains(self.poolID) {
+                // if not create an empty LP Token
+                let tokenVault <- EmuSwap.createEmptyTokenVault(tokenID: self.poolID)
+                self.lpCollectionRef.deposit(token: <-tokenVault)
+                self.liquidityTokenRef = self.lpCollectionRef.borrowVault(id: self.poolID)
+                }
+                
+                self.liquidityTokenRef = self.lpCollectionRef.borrowVault(id: self.poolID)
+            }
+            
+            execute {
+                // Withdraw tokens
+                let token1Vault <- self.flowTokenVaultRef.withdraw(amount: token1Amount) as! @FlowToken.Vault
+                let token2Vault <- self.fusdVaultRef.withdraw(amount: token2Amount) as! @FUSD.Vault
+            
+                // create a token bundle with both tokens in equal measure
+                let tokenBundle <- EmuSwap.createTokenBundle(fromToken1: <- token1Vault, fromToken2: <- token2Vault)
+                
+                // Pass tokenbundle to add liquidity and get LP tokens in return
+                let liquidityTokenVault <- EmuSwap.borrowPool(id: self.poolID)?.addLiquidity!(from: <- tokenBundle)
+            
+                // Deposit the liquidity provider tokens
+                self.liquidityTokenRef.deposit(from: <- liquidityTokenVault)
             }
         }
         `,
